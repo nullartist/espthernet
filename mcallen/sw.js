7d5db0a4b6754976b2d3b48063647a4bf234a067
nullartist <victor128@hotmail.com>
2026-07-29 18:13:41 -0600

    Agrega funcionamiento offline de McAllen Go

---
+++ mcallen/sw.js
@@ -0,0 +1,5 @@
+const CACHE='mcallen-go-v3.0.0';
+const CORE=["./", "./index.html", "./manifest.webmanifest", "./icon.svg", "./core.css", "./visual.css", "./loader.js", "./ui-hero.html", "./ui-home.html", "./ui-route.html", "./ui-content.html", "./ui-overlays.html", "./data.js", "./core.js", "./features.js", "./share.js"];
+self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(CORE)).then(()=>self.skipWaiting())));
+self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(ks=>Promise.all(ks.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
+self.addEventListener('fetch',e=>{if(e.request.method!=='GET')return;const u=new URL(e.request.url);if(e.request.mode==='navigate'){e.respondWith(fetch(e.request).then(r=>{const c=r.clone();caches.open(CACHE).then(x=>x.put('./index.html',c));return r}).catch(()=>caches.match('./index.html')));return}if(u.origin===location.origin)e.respondWith(caches.match(e.request).then(c=>c||fetch(e.request).then(r=>{const x=r.clone();caches.open(CACHE).then(k=>k.put(e.request,x));return r}))) });
